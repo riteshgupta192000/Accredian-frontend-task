f35ab3e502ab482ed47904866111002183b21011
riteshgupta192000 <riteshgupta192000@gmail.com>
2025-02-15 18:59:25 +0530

    added production link

--- src/utils/AxiosIntance.js
+++ src/utils/AxiosIntance.js
@@ -1,7 +1,20 @@
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//     baseURL: "http://localhost:5000",
+// });
+
+// export default axiosInstance;
+
 import axios from "axios";
 
+const isProduction = window.location.hostname !== "localhost";
+
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: isProduction
+        ? "https://accredian-backend-refer.onrender.com"
+        : "http://localhost:5000",
 });
 
 export default axiosInstance;
+
